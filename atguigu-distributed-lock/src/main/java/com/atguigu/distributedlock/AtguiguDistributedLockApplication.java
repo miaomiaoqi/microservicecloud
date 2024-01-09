1e5bd4e53f53624747eff8311d163f99407d2b3f
miaoqi <miaoqi3@xdf.cn>
2024-01-09 18:27:22 +0800

    update blog

---
+++ atguigu-distributed-lock/src/main/java/com/atguigu/distributedlock/AtguiguDistributedLockApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguDistributedLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtguiguDistributedLockApplication.class, args);
+	}
+
+}
---
+++ atguigu-distributed-lock/src/test/java/com/atguigu/distributedlock/AtguiguDistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.distributedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtguiguDistributedLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
